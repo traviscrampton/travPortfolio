4d3596aab9c4a5cc10574509bbd23e6c41d83a1e
Travis Crampton <crampton.travis@gmail.com>
2015-12-01 14:56:32 -0800

    create blog model w/ no associations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,12 @@
 class Blog < ActiveRecord::Base
-  
+
+  validates :title, :presence => true
+  validates :date, :presence => true
+  validates :distance, :presence => true
+  validates :time, :presence => true
+
+  has_attached_file :mapimage, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+validates_attachment_content_type :mapimage, :content_type => /\Aimage\/.*\Z/
+
+
 end
