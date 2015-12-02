d58ba743676ea4b46a4761e0a625d1c7daa31103
Travis Crampton <crampton.travis@gmail.com>
2015-12-02 14:00:00 -0800

    still formatting blog page

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,7 +5,7 @@ class Blog < ActiveRecord::Base
   validates :distance, :presence => true
   validates :time, :presence => true
 
-  has_attached_file :mapimage, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :mapimage, :styles => { :medium => "200x200>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
 validates_attachment_content_type :mapimage, :content_type => /\Aimage\/.*\Z/
 
   has_many :pics
