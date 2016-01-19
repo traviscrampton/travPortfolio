f8a4b2f3b1fad1e7e9db34173bb98d4f61941890
Travis Crampton <crampton.travis@gmail.com>
2016-01-19 10:23:21 -0800

    changed font

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,6 +1,6 @@
 class Pic < ActiveRecord::Base
 
-  has_attached_file :photo, :styles => { :standard => "800x533", :small => "450x300"}, :default_url => "/images/:style/missing.png"
+  has_attached_file :photo, :styles => { :standard => "1000x533", :small => "450x300"}, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
   has_one :descript, dependent: :destroy
