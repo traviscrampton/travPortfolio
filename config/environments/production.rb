3f70e21b4cc6488da2012bcdbc23b85788291f90
Travis Crampton <crampton.travis@gmail.com>
2016-01-21 15:50:13 -0800

    production rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,7 @@ Rails.application.configure do
   # NGINX, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  config.serve_static_assets = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -30,7 +31,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
