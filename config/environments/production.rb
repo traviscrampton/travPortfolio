d94b29ebbc78498567ddf8a0b0255f77d9b3f45f
Travis Crampton <crampton.travis@gmail.com>
2016-01-21 18:27:13 -0800

    works

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,11 +79,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { :host => "https://evening-harbor-24338.herokuapp.com" }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'evening-harbor-24338.herokuapp.com',
+    domain:               'https://evening-harbor-24338.herokuapp.com',
     user_name:            'crampton.travis@gmail.com',
     password:             'shlank08',
     authentication:       'plain',
