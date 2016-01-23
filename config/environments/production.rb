cbc29466861357194de37cb2d1fb31217de42185
Travis Crampton <crampton.travis@gmail.com>
2016-01-23 13:41:02 -0800

    help

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,13 +79,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => "http://traviscrampton.xyz" }
+  config.action_mailer.default_url_options = { :host => "traviscrampton.xyz" }
 
   config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
-      domain:               'http://traviscrampton.xyz',
+      domain:               'traviscrampton.xyz',
       user_name:            'travisappdomain@gmail.com',
       password:             'entersandman',
       authentication:       'plain',
